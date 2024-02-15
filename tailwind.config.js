ced6f214664b264f89ee23b4f7fdd3cbe7b8e153
Carolina Sasowsky <mc.sasowsky@gmail.com>
2024-02-15 13:49:25 -0300

    estructura navbar del proyecto web AhorrADAs

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+	darkMode: "class",
+	theme: {
+		screens: {
+			sm: "600px",
+			md: "860px",
+			lg: "1024px",
+		},
+		colors: {
+				white: "#FFFFFF",
+				purple: "#3F3CBB",
+				midnight: "#121063",
+				metal: "#565584",
+				"tahiti-blue": "#3AB7BF",
+				"cool-white": "#ECEBFF",
+				"bubble-gum": "#FF77E9",
+				"cooper-rust": "#78DCCA",
+		},
+	},
+}
+
