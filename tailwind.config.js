cd069701c89c3a3bd906c5a45fed09a2055f96aa
Carolina Sasowsky <mc.sasowsky@gmail.com>
2024-05-06 14:47:37 -0300

    se corrigieron todos los errores de etiqueta del html

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,18 +22,5 @@ tailwind.config = {
 			md: "860px",
 			lg: "1024px",
 		},
-		extend: {
-			fontFamily: {},
-			colors: {
-				white: "#FFFFFF",
-				purple: "#3F3CBB",
-				midnight: "#121063",
-				metal: "#565584",
-				"tahiti-blue": "#3AB7BF",
-				"cool-white": "#ECEBFF",
-				"bubble-gum": "#FF77E9",
-				"cooper-rust": "#78DCCA",
-			},
-		},
 	},
 };
