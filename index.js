a048042eb7afc547c2a7f8ff1f640538cf244f63
Carolina Sasowsky <mc.sasowsky@gmail.com>
2024-12-10 14:29:05 -0300

    construccion de las cards  para cada seccion del proyecto

---
+++ index.js
@@ -0,0 +1,17 @@
+/* ///////////////////////////////////////// ***ELEMENTOS***/////////////////////////////////////*/
+
+// Maquetado
+
+const linkBalance = document.querySelector(".-balance");
+const linkCategorias = document.querySelector(".link-categorias");
+const linkReportes = document.querySelector(".link-reportes");
+const seccionPrincipal = document.querySelector("#principal");
+const seccionCategorias = document.querySelector("#seccion-categorias");
+const seccionReportes = document.querySelector(".reportes");
+const menuDesplegableMobile = document.querySelector(
+	"#menu-desplegable-mobile"
+);
+const botonHamburguesa = document.getElementById("boton-hamburguesa");
+const seccionNuevaOperacion = document.getElementById(
+	"formulario-nueva-operacion"
+);
