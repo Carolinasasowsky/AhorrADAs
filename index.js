cb29bf090f65c5bc826270033bb8b5b674483446
Carolina Sasowsky <mc.sasowsky@gmail.com>
2025-01-29 19:34:16 -0300

    Se implementaron los media queries para la barra de navegación y los paneles de Balance, Filtros y Operaciones, ajustando el diseño en versiones móviles y desktop. Además, se eliminaron configuraciones innecesarias.

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const linkReportes = document.querySelector(".link-reportes");
 const seccionPrincipal = document.querySelector("#principal");
 const seccionCategorias = document.querySelector("#seccion-categorias");
 const seccionReportes = document.querySelector(".reportes");
-const menuDesplegableMobile = document.querySelector(
-	"#menu-desplegable-mobile"
-);
+const menuDesplegableMobile = document.querySelector("#menu-desplegable-mobile");
 const botonHamburguesa = document.getElementById("boton-hamburguesa");
-const seccionNuevaOperacion = document.getElementById(
-	"formulario-nueva-operacion"
-);
+const seccionNuevaOperacion = document.getElementById("formulario-nueva-operacion");
+
